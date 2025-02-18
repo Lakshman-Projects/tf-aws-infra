4d5da127e1e7db5c1a6f6d2d9b210272af4b7b2d
Lakshman Siva <slakshman664@gmail.com>
2025-02-18 00:32:24 -0500

    Update variables.tf for subnets and AZs
    Modified variables.tf to define variables for public and private subnets, as well as availability zones for the VPC configuration.

--- variables.tf
+++ variables.tf
@@ -12,10 +12,28 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "VPC CIDR Block"
   type        = string
-  default     = "10.0.0.0/24"
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+  default     = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
 }
