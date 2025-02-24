f46bdeb435ad21989ae05864f8420361cea36ff3
Lakshman Siva <slakshman664@gmail.com>
2025-02-24 18:33:43 -0500

    test commit 3

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count          = length(var.public_subnets)
+  cout              = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
