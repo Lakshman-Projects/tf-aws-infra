d25c607b5213b3358e343836993b011276d9ec50
Lakshman Siva <siva.l@northeastern.edu>
2025-03-16 00:13:13 -0400

    Attach IAM role and policy to EC2 instance for S3 access
    Added Terraform code to attach an IAM role to the EC2 instance with a policy that grants necessary permissions for accessing S3 buckets. Ensured the policy follows the principle of least privilege for secure access.

--- main.tf
+++ main.tf
@@ -89,26 +89,6 @@ resource "aws_security_group" "app_security_group" {
   }
 }
 
-resource "aws_instance" "app_instance" {
-  ami                         = var.custom_ami_id
-  instance_type               = "t2.micro"
-  key_name                    = var.key_name
-  subnet_id                   = aws_subnet.public[0].id
-  security_groups             = [aws_security_group.app_security_group.id]
-  associate_public_ip_address = true
-
-  root_block_device {
-    volume_size           = 25
-    volume_type           = "gp2"
-    delete_on_termination = true
-  }
-
-  disable_api_termination = false
-  tags = {
-    Name = "App EC2 Instance"
-  }
-}
-
 resource "random_uuid" "bucket_suffix" {}
 
 resource "aws_s3_bucket" "bucket" {
@@ -263,3 +243,32 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_s3_role.name
 }
 
+resource "aws_instance" "app_instance" {
+  ami                         = var.custom_ami_id
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.public[0].id
+  security_groups             = [aws_security_group.app_security_group.id]
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+
+  root_block_device {
+    volume_size           = 25
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "DB_HOST=${aws_db_instance.main.endpoint}" >> /etc/environment
+    echo "DB_USER=csye6225" >> /etc/environment
+    echo "DB_PASS=${var.db_password}" >> /etc/environment
+    echo "DB_NAME=csye6225" >> /etc/environment
+    echo "S3_BUCKET=${aws_s3_bucket.private_bucket.bucket}" >> /etc/environment
+    EOF
+
+  disable_api_termination = false
+  tags = {
+    Name = "App EC2 Instance"
+  }
+}
